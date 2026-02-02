e4965e36f215f132f8f7e32ea7e4fd21836ad109
CoderLinxin <1633954829@qq.com>
2026-02-02 20:14:51 +0800

    2026-02-02

--- test_experiment.py
+++ test_experiment.py
@@ -120,7 +120,7 @@ def get_bicubic_lr(hr_path: str):
 # )
 main(
     model_type='hit_sir_pro',
-    weight_path='weights/hitsir_pro测试浅层特征提取3_loss(l1)_mulsizeconvextract(True)_casa(True)_fusion_embed_dim(180)_len(depths)(6)_augment/best_psnr_ssim_lpips_model.pth',
+    weight_path='weights/hitsir_pro测试浅层特征提取3_loss(l1)_mulsizeconvextract(True)_casa(True)_fusion_embed_dim(180)_len(depths)(6)_augment/best_ssim_model.pth',
     img_path='data/test/RealSRSet+5images/0014.jpg'
 )
 # main(
