77cc380ac1710898d9acb5cdec7ef1a7707f4ecb
CoderLinxin <1633954829@qq.com>
2026-03-03 06:00:51 +0800

    2026-03-03

--- test_experiment.py
+++ test_experiment.py
@@ -126,7 +126,7 @@ def get_bicubic_lr(hr_path: str):
 main(
     model_type='hit_sir_pro',
     weight_path='weights/hitsir_pro测试浅层特征提取3_gan_loss(l1)_mulsizeconvextract(True)_casa(True)_fusion_embed_dim(180)_len(depths)(6)_augment/new_epoch_model.pth',
-    img_path='data/test/网上图片/16.jpg'
+    img_path='data/test/RealSRSet+5images/0014.jpg'
 )
 # main(
 #     model_type='bsrgan',
