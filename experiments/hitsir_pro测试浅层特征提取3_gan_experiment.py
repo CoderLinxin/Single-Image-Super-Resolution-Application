79ddb7683513998df6991350b5f499a01d1195cb
CoderLinxin <1633954829@qq.com>
2026-03-07 07:31:57 +0800

    2026-03-07

--- experiments/hitsir_pro测试浅层特征提取3_gan_experiment.py
+++ experiments/hitsir_pro测试浅层特征提取3_gan_experiment.py
@@ -312,12 +312,12 @@ def hitsir_pro测试浅层特征提取3_gan_experiment(
             # '城市风景', '城市泊油路', '迪拜旅游城市', '日本庭院', '铁轨铁路', '乌克兰城市建筑', '自然风景',
 
             # 训练集7(针对现实世界图像超分)
-            # 'blend', 'RealSR(V3)', 'DIV2K_train_HR', 'Flickr2K_HR', 'face', 'REDS2K',
-            # '城市风景', '城市泊油路', '迪拜旅游城市', '日本庭院', '铁轨铁路', '乌克兰城市建筑', '自然风景',
-            'wed1', 'wed2', 'wed3', 'wed4',
-            'OST_dataset/animal', 'OST_dataset/building',
-            'OST_dataset/grass', 'OST_dataset/mountain',
-            'OST_dataset/plant', 'OST_dataset/sky', 'OST_dataset/water',
+            'blend', 'RealSR(V3)', 'DIV2K_train_HR', 'Flickr2K_HR', 'face', 'REDS2K',
+            '城市风景', '城市泊油路', '迪拜旅游城市', '日本庭院', '铁轨铁路', '乌克兰城市建筑', '自然风景',
+            # 'wed1', 'wed2', 'wed3', 'wed4',
+            # 'OST_dataset/animal', 'OST_dataset/building',
+            # 'OST_dataset/grass', 'OST_dataset/mountain',
+            # 'OST_dataset/plant', 'OST_dataset/sky', 'OST_dataset/water',
 
             # 训练集8(针对动漫图像超分)
             # 'wuthering_wave', 'Z1500PX', '2000PXA', '2000PXB', '2000PXC', '2000PXE', *(['动漫图片'] * 5),
