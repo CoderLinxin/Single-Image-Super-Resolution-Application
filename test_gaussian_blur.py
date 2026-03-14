12400f0c04e748a1ef5f1a4b35ad1c0cbd9a2e1f
CoderLinxin <1633954829@qq.com>
2026-03-15 00:12:18 +0800

    2026-03-15

--- test_gaussian_blur.py
+++ test_gaussian_blur.py
@@ -7,5 +7,6 @@ hr_path = 'data/test/Set5/baby_GT.bmp'
 with PIL.Image.open(hr_path, mode='r') as img_open:
     hr = img_open.convert('RGB')
 
-lr = hr.filter(ImageFilter.GaussianBlur(radius=2))
+lr = hr.filter(ImageFilter.GaussianBlur(radius=5))
+# lr = hr.filter(ImageFilter.BoxBlur(radius=5))
 lr.show()
