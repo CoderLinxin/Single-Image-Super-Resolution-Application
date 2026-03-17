7b55c3d9ae9b2838e2f3f3c8c2ed13d7af5bad8f
CoderLinxin <1633954829@qq.com>
2026-03-17 20:40:59 +0800

    2026-03-17

--- test_gaussian_blur.py
+++ test_gaussian_blur.py
@@ -1,12 +1,21 @@
 import PIL.Image
 from os import path
 from PIL import Image, ImageFilter
+from torchvision.transforms import transforms
 
 hr_path = 'data/test/Set5/baby_GT.bmp'
 
 with PIL.Image.open(hr_path, mode='r') as img_open:
     hr = img_open.convert('RGB')
 
-lr = hr.filter(ImageFilter.GaussianBlur(radius=5))
+# lr = hr.filter(ImageFilter.GaussianBlur(radius=5))
 # lr = hr.filter(ImageFilter.BoxBlur(radius=5))
-lr.show()
+# lr.show()
+
+
+to_tensor_transform = transforms.ToTensor()
+to_pil_transform = transforms.ToPILImage()
+
+hazy = (to_tensor_transform(hr) * 0.7 + 80 / 255.).clip(0, 1)
+hazy = to_pil_transform(hazy)
+hazy.show()
